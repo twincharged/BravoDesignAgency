74580ef45890a3181b5391b2ab976291a17900c0
twincharged <jcrgtr@gmail.com>
2014-12-10 14:42:46 -0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-var actives = document.querySelectorAll("a[href='"+window.location.pathname+"']")
+var actives = document.querySelectorAll("a[href='"+window.location.pathname.slice(5)+"']")
 
 for (var i=0;i<actives.length;i++) {
   actives[i].classList.add("active-link")
