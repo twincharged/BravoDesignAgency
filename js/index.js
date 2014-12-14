758f423ba2ad12be9996bcce88169126a71fdde9
Joey <jcrgtr@gmail.com>
2014-12-14 04:36:07 -0700

    JS fixes.

--- js/index.js
+++ js/index.js
@@ -1,5 +1,7 @@
-var loc = window.location.pathname.split("/").slice(-1)||"index.html",
-    actives = document.querySelectorAll("a[href]")
+var loc = window.location.pathname.split("/").slice(-1),
+    actives = document.querySelectorAll("header a[href]")
+
+loc = (loc[0] == "") ? "index.html" : loc
 
 for (var i=0;i<actives.length;i++) {
   var a = actives[i]
